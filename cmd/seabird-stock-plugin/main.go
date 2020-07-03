317ab132e775961fec81658ef4f03b94eff0161e
Jared Ledvina <jared@techsmix.net>
2020-07-03 18:49:12 -0400

    Quick hack for Google Cloud Run
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- cmd/seabird-stock-plugin/main.go
+++ cmd/seabird-stock-plugin/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/jaredledvina/seabird-stock-plugin"
+	stock "github.com/jaredledvina/seabird-stock-plugin"
 	"github.com/joho/godotenv"
 	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
 )
+
+// HTTPHandler create a handler struct
+type HTTPHandler struct{}
+
+// implement `ServeHTTP` method on `HttpHandler` struct
+func (h HTTPHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+	// create response binary data
+	data := []byte("Hello World!") // slice of bytes
+
+	// write `data` to response
+	res.Write(data)
+}
 
 func main() {
 	// Attempt to load from .env if it exists
@@ -28,6 +42,7 @@ func main() {
 	coreURL := os.Getenv("SEABIRD_HOST")
 	coreToken := os.Getenv("SEABIRD_TOKEN")
 	finnhubToken := os.Getenv("FINNHUB_TOKEN")
+	servicePort := os.Getenv("PORT")
 
 	if coreURL == "" || coreToken == "" {
 		log.Fatal("Missing SEABIRD_HOST or SEABIRD_TOKEN")
@@ -41,6 +56,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// create a new handler
+	handler := HTTPHandler{}
+
+	// listen and serve
+	go http.ListenAndServe(servicePort, handler)
 
 	err = c.Run()
 	if err != nil {
