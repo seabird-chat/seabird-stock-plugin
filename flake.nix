7def61fc06b1f12dd4044c253dfc16fe4888cb27
Kaleb Elwert <belak@coded.io>
2026-08-11 14:29:20 -0700

    chore: bump to v0.6.2-dev

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
           packages.default = pkgs.buildGoModule rec {
             pname = "seabird-stock-plugin";
-            version = "0.6.1";
+            version = "0.6.2-dev";
 
             src = ./.;
 
